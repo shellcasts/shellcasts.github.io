a06dcf1c89a7e38980179f5e6b4e0a8cbacd7301
Tjaart van der Walt <git@tjaart.org>
2021-11-29 21:21:32 -0800

    Add vite import alias

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import mdPlugin, {Mode} from 'vite-plugin-markdown'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import mdPlugin, { Mode } from "vite-plugin-markdown"
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), mdPlugin({ mode: [Mode.HTML, Mode.TOC, Mode.VUE] })]
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  plugins: [vue(), mdPlugin({ mode: [Mode.HTML, Mode.TOC, Mode.VUE] })],
 })
