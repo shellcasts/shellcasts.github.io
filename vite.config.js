ffb7804cb5fc61e2dcfa428c4d433e773799bfd4
Tjaart van der Walt <git@tjaart.org>
2021-12-24 11:15:58 -0800

    Include markdown in build

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  assetsInclude: ['**/*.md'],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
